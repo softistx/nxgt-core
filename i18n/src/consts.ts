fabb49a75c7e6c382c967140d7776b919972cdfa
Steve Tsala <stevegires@gmail.com>
2026-03-13 09:28:55 +0100

    update: Update i18n messages

--- i18n/src/consts.ts
+++ i18n/src/consts.ts
@@ -1,1 +1,8 @@
+import { resources } from './resources';
+import type { Language } from './types';
+
 export const LANGUAGE_KEY = 'language';
+
+export const SUPPORTED_LANGUAGES = Object.keys(resources) as Language[];
+
+export const FALLBACK_LANGUAGE: Language = 'en';
--- i18n/src/i18n.ts
+++ i18n/src/i18n.ts
@@ -3,15 +3,30 @@ import { logger } from '@nxgt/shared/logging';
 import { tryGetContext } from 'hono/context-storage';
 import { IntlMessageFormat } from 'intl-messageformat';
 import _ from 'lodash';
+import { FALLBACK_LANGUAGE, LANGUAGE_KEY } from './consts';
 import { resources } from './resources';
-import type { LocaleKey, TranslationContext } from './types';
+import type { Language, LocaleKey, TranslationContext } from './types';
+
+export function getLanguage() {
+	if (typeof localStorage === 'undefined') {
+		return (
+			tryGetContext()?.get(LANGUAGE_KEY) === 'fr' ? 'fr' : FALLBACK_LANGUAGE
+		) as Language;
+	}
+	return (
+		localStorage.getItem(LANGUAGE_KEY) === 'fr' ? 'fr' : FALLBACK_LANGUAGE
+	) as Language;
+}
 
 export function createTranslator<K extends string = LocaleKey>(
 	resources: Record<string, any> = {},
+	localeProvider: () => Language = getLanguage,
 ) {
-	return (key: K, context: TranslationContext = undefined) => {
-		const language = tryGetContext()?.get('language' as never) ?? 'en';
-
+	return (
+		key: K,
+		context: TranslationContext = undefined,
+		language: Language = localeProvider(),
+	) => {
 		let message: string = _.get(resources[language], key) ?? key;
 		try {
 			message = cast(new IntlMessageFormat(message, language).format(context));
