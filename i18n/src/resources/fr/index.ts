f2f822dcdb19d1382d442a5ddd1509a1d8475e4d
Steve Tsala <stevegires@gmail.com>
2026-03-18 21:06:15 +0100

    update: Update i18n

--- i18n/src/resources/fr/index.ts
+++ i18n/src/resources/fr/index.ts
@@ -1,9 +1,11 @@
 import common from './common.json';
 import errors from './errors.json';
 import validation from './validation.json';
+import zod from './zod.json';
 
 export const fr = {
 	...common,
 	errors,
 	validation,
+	zod,
 };
